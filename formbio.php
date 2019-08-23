653f1141b6df3b6661a274bd9212e190ab10af95
farrelfdlhs <farrelsanan12@gmail.com>
2019-08-23 07:57:05 +0700

    Form Biodata & Prosesnya

--- formbio.php
+++ formbio.php
@@ -10,38 +10,102 @@
     <fieldset>
         <legend align="center"><h1>Formulir Pendaftaran</h1></legend><br>
         <form action="prosesformbio.php" method="POST" name="input">
-            NO PNENDAFTARAN : <input type="number" name="nomor"><br><br>
-            NISN            : <input type="number" name="nisn"><br><br>
-            NAMA            : <input type="text" name="nama"><br><br>
-            ALAMAT          : <textarea name="alamat" cols="30" rows="5"></textarea><br><br>
-            TEMPAT LAHIR    : <input type="text" name="tl"><br><br>
-            TANGGAL LAHIR   : <input type="date" name="ttl"><br><br>
-            ASAL SEKOLAH    : <input type="text" name="sekolah"><br><br>
-            JENIS KELAMIN   : <input type="radio" name="jenkel" value="Laki Laki"> Laki Laki <br>
-                              <input type="radio" name="jenkel" value="Perempuan"> Perempuan <br>
-            JURUSAN         : <select name="jurusan">
+        <table border=0 align="center"> 
+            <tr>
+                <td>NO PENDAFTARAN</td>
+                <td>:</td> 
+                <td><input type="number" name="nomor"><br><br></td>
+            </tr>
+            <tr>
+                <td>NISN</td>            
+                <td>:</td> 
+                <td><input type="number" name="nisn"><br><br></td>
+            </tr>
+            <tr>
+                <td>NAMA</td>             
+                <td>:</td>
+                <td><input type="text" name="nama"><br><br></td>
+            </tr>
+            <tr>
+                <td>ALAMAT</td>           
+                <td>:</td>
+                <td><textarea name="alamat" cols="30" rows="5"></textarea><br><br></td>
+            </tr>
+            <tr>
+                <td>TEMPAT LAHIR</td>     
+                <td>:</td> 
+                <td><input type="text" name="tl"><br><br></td>
+            </tr>
+            <tr>
+                <td>TANGGAL LAHIR</td>    
+                <td>:</td>
+                <td><input type="date" name="ttl"><br><br></td>
+            </tr>
+            <tr>
+                <td>ASAL SEKOLAH</td>     
+                <td>:</td>
+                <td><input type="text" name="sekolah"><br><br></td>
+            </tr>
+            <tr>
+                <td>JENIS KELAMIN</td>    
+                <td>:</td> 
+                <td><input type="radio" name="jenkel" value="Laki Laki"> Laki Laki <br>
+                <input type="radio" name="jenkel" value="Perempuan"> Perempuan <br></td>
+            </tr>
+            <tr>
+                <td>JURUSAN</td>          
+                <td>:</td> 
+                <td><select name="jurusan">
                                 <option value="Rekayasa Perangkat Lunak">RPL</option>
                                 <option value="Teknik Sepeda Motor">TSM</option>
                                 <option value="Teknik Kendaraan Ringan">TKR</option>
-                              </select><br><br>
-            NAMA AYAH       : <input type="text" name="namay" ><br><br>
-            PEKERJAAN AYAH  : <input type="text" name="keray" ><br><br>
-            NAMA IBU        : <input type="text" name="namib"><br><br>
-            PEKERJAAN IBU   : <input type="text" name="kerib"><br><br>
-            AGAMA           : <select name="agama">
+                    </select><br><br></td>
+            </tr>
+            <tr>
+                <td>NAMA AYAH</td>        
+                <td>:</td>
+                <td><input type="text" name="namay" ><br><br></td>
+            </tr>
+            <tr>
+                <td>PEKERJAAN AYAH</td> 
+                <td>:</td> 
+                <td><input type="text" name="keray" ><br><br></td>
+            </tr>
+            <tr>
+                <td>NAMA IBU</td>         
+                <td>:</td> 
+                <td><input type="text" name="namib"><br><br></td> 
+            </tr>
+            <tr>
+                <td>PEKERJAAN IBU</td>    
+                <td>:</td> 
+                <td><input type="text" name="kerib"><br><br></td> 
+            </tr>
+            <tr>
+                <td>AGAMA</td>            
+                <td>:</td> 
+                <td><select name="agama">
                                 <option value="Islam">Islam</option>
                                 <option value="Kristen">Kristen</option>
                                 <option value="Hindu">Hindu</option>
                                 <option value="Budha">Budha</option>
                                 <option value="Ateis">Ateis</option>
-                              </select><br><br>
-            HOBBY           : <input type="checkbox" name="ngegame" value="Main Game">Main Game
-                              <input type="checkbox" name="Ngusep" value="Ngusep">Mancing
-                              <input type="checkbox" name="Membaca" value="Membaca">Membaca
-                              <input type="checkbox" name="Menggambar" value="Menggambar">Menggambar
-                              <input type="checkbox" name="Melukis" value="Melukis">Melukis <br><br>
-            <input type="submit" name="simpan" value="SIMPAN">
-            <input type="reset" name="reset" value="KEMBALI">
+                              </select><br><br></td> 
+            </tr>
+            <tr>
+                <td>HOBBY</td>            
+                <td>:</td>
+                <td><input type="checkbox" name="ngegame" value="Main Game">Main Game
+                <input type="checkbox" name="Ngusep" value="Ngusep">Mancing
+                <input type="checkbox" name="Membaca" value="Membaca">Membaca
+                <input type="checkbox" name="Menggambar" value="Menggambar">Menggambar
+                <input type="checkbox" name="Melukis" value="Melukis">Melukis <br></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="simpan" value="SIMPAN"> 
+                <input type="reset" name="reset" value="KEMBALI"></td>
+            </tr>
+            </table>
         </form>
     </fieldset>
 </body>
--- prosesformbio.php
+++ prosesformbio.php
@@ -12,6 +12,34 @@
         echo "TEMPAT LAHIR      : $tempat<br>";
         $tanggal = $_POST['ttl'];
         echo "TANGGAL LAHIR     : $tanggal<br>";
+        $sekul = $_POST['sekolah'];
+        echo "ASAL SEKOLAH      : $sekul<br>";
+        $jk = $_POST['jenkel'];
+        echo "JENIS KELAMIN     : $jk<br>";
+        $jurus = $_POST['jurusan'];
+        echo "JURUSAN           : $jurus<br>";
+        $ayah = $_POST['namay'];
+        echo "NAMA AYAH         : $ayah<br>";
+        $gaweay = $_POST['keray'];
+        echo "PEKERJAAN AYAH    : $gaweay<br>";
+        $ibu = $_POST['namib'];
+        echo "NAMA IBU          : $ibu<br>";
+        $gaweib = $_POST['kerib'];
+        echo "PEKERJAAN IBU     : $gaweib<br>";
+        $pray = $_POST['agama'];
+        echo "AGAMA             : $pray<br>";
+    }
+
+    if (isset($_POST['ngegame'])) {
+        echo "HOBBY             : ".$_POST['ngegame'];
+    } if (isset($_POST['Ngusep'])) {
+        echo "HOBBY             : ".$_POST['Ngusep'];
+    } if (isset($_POST['Membaca'])) {
+        echo "HOBBY             : ".$_POST['Membaca'];
+    } if (isset($_POST['Menggambar'])) {
+        echo "HOBBY             : ".$_POST['Menggambar'];
+    } if (isset($_POST['Melukis'])) {
+        echo "HOBBY             : ".$_POST['Melukis'];
     }
 
 
